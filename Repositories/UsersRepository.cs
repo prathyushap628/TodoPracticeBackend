1bd0612614a269f1b24385a73c7c2437ad321e51
Sriramula prathyusha <96115648+prathyushap628@users.noreply.github.com>
2022-04-18 18:12:09 +0530

    backend

---
+++ Repositories/UsersRepository.cs
@@ -0,0 +1,27 @@
+using Todo.Models;
+using Dapper;
+using Todo.Utilities;
+using ToDoApp.Repositories;
+
+namespace Todo.Repositories;
+
+public interface IUserRepository
+{
+    Task<User> GetByUsername(string Username);
+}
+public class UserRepository : BaseRepository, IUserRepository
+{
+    public UserRepository(IConfiguration config) : base(config)
+    {
+
+    }
+
+    public async Task<User> GetByUsername(string Username)
+    {
+        var query = $@"SELECT * FROM ""{TableNames.users}"" 
+        WHERE username = @Username";
+
+        using (var con = NewConnection)
+            return await con.QuerySingleOrDefaultAsync<User>(query, new { Username });
+    }
+}
